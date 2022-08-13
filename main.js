0703d7d71a42dfb619641ee6e5e592b8deaae0b3
Kishore Kevin <98908744+KISHOREkevin@users.noreply.github.com>
2022-08-13 11:08:46 +0530

    upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var hours= document.querySelector(".hours");
+var minutes = document.querySelector(".minutes");
+var seconds = document.querySelector(".seconds");
+var ampm=document.querySelector(".ampm")
+
+
+
+function updateTime(){
+	var time = new Date();
+	var hrs = time.getHours();
+	var min = time.getMinutes();
+	var sec = time.getSeconds();
+	if(hrs>12){
+		hrs=hrs-12;
+		ampm.textContent="PM"
+	}
+	hours.textContent=hrs;
+	minutes.textContent=min;
+	seconds.textContent=sec;
+	setTimeout(function() {
+		updateTime();
+	},1000);
+
+}
+
+updateTime();
